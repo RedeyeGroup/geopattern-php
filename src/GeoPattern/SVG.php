0a89746495902aa5a09617a6fc5cccd8ede3bb53
Sam-Mauris Yong <mauris@hotmail.sg>
2014-04-18 14:49:09 +0800

    SVG class PSR-2 compliant

--- src/GeoPattern/SVG.php
+++ src/GeoPattern/SVG.php
@@ -6,13 +6,13 @@ use GeoPattern\SVGElements\Rectangle;
 use GeoPattern\SVGElements\Circle;
 use GeoPattern\SVGElements\Path;
 
-class SVG {
-
+class SVG
+{
     protected $width;
     protected $height;
     protected $svgString;
 
-    function __construct($options=array())
+    public function __construct($options = array())
     {
         $this->width = 100;
         $this->height = 100;
@@ -41,38 +41,37 @@ class SVG {
         return '</svg>';
     }
 
-    public function addRectangle($x, $y, $width, $height, $args=array())
+    public function addRectangle($x, $y, $width, $height, $args = array())
     {
         $rectangle = new Rectangle($x, $y, $width, $height, $args);
         $this->svgString .= $rectangle;
         return $this;
     }
 
-    public function addCircle($cx, $cy, $r, $args=array())
+    public function addCircle($cx, $cy, $r, $args = array())
     {
         $circle = new Circle($cx, $cy, $r, $args);
         $this->svgString .= $circle;
         return $this;
     }
 
-    public function addPath($d, $args=array())
+    public function addPath($d, $args = array())
     {
         $path = new Path($d, $args);
         $this->svgString .= $path;
         return $this;
     }
 
-    public function addPolyline($points, $args=array())
+    public function addPolyline($points, $args = array())
     {
         $polyline = new Polyline($points, $args);
         $this->svgString .= $polyline;
         return $this;
     }
 
-    public function addGroup($group, $args=array())
+    public function addGroup($group, $args = array())
     {
-        if ($group instanceof Group)
-        {
+        if ($group instanceof Group) {
             $group->setArgs($args);
             $this->svgString .= $group;
             return $this;
@@ -82,13 +81,11 @@ class SVG {
 
     public function getString()
     {
-        return $this->getSvgHeader().$this->svgString.$this->getSvgFooter();
+        return $this->getSvgHeader() . $this->svgString . $this->getSvgFooter();
     }
 
     public function __toString()
     {
         return $this->getString();
     }
-
-
 }
